476481af130a0da4126643c2dcb812c164c23b80
Mohid Makhdoomi <3915465+mohidmakhdoomi@users.noreply.github.com>
2020-09-14 20:39:20 -0400

    Optimization

--- Main/nvidia_bot.py
+++ Main/nvidia_bot.py
@@ -1,7 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
-import time
+from datetime import datetime
+
+
+class ElementHasPartial(object):
+    def __init__(self, partial_val):
+        self.partial_val = partial_val
+
+    def __call__(self, driver):
+        try:
+            return driver.find_element_by_partial_link_text(self.partial_val)
+        except NoSuchElementException:
+            return False
 
 
 class ElementHasXpath(object):
@@ -40,23 +51,22 @@ class ElementHasId(object):
 def run(username, password, cvv):
     continue_xpath = "//input[@type='submit' and @value='continue']"
     submit_xpath = "//input[@type='submit' and @value='submit']"
+    print('Starting Now')
+    print(datetime.now())
     driver = webdriver.Chrome('Main\chromedriver.exe')
     wait = WebDriverWait(driver, 15, poll_frequency=0.01)
-    driver.implicitly_wait(10)
+    wait_add = WebDriverWait(driver, 5, poll_frequency=0.01)
     driver.get('https://store.nvidia.com/DRHM/store?Action=Logout&SiteID=nvidia&Locale=en_US&ThemeID=326200&Env=BASE&nextAction=help')
-    driver.find_element_by_id('loginEmail').send_keys(str(username))
-    pass_text = driver.find_element_by_id('loginPassword')
+    wait.until(ElementHasId('loginEmail')).send_keys(str(username))
+    pass_text = wait.until(ElementHasId('loginPassword'))
     pass_text.send_keys(str(password))
     pass_text.submit()
     driver.get("https://www.nvidia.com/en-us/shop/geforce/gpu/?page=1&limit=1&locale=en-us&category=GPU&search=NVIDIA%20GEFORCE%20RTX%203080")
-    driver.implicitly_wait(0)
     while True:
         try:
-            add_to_cart_button = driver.find_element_by_partial_link_text("CART")  # driver.find_element_by_class_name('cta-button').click()
-        except NoSuchElementException:
-            time.sleep(2)
+            add_to_cart_button = wait_add.until(ElementHasPartial('CART'))
+        except TimeoutException:
             driver.execute_script("window.location.reload(true);")
-            time.sleep(8)
         else:
             add_to_cart_button.click()
             break
@@ -75,11 +85,10 @@ def run(username, password, cvv):
             else:
                 card_button.click()
                 wait.until(ElementHasXpath(continue_xpath)).click()
-                print('selected card!!!!!!!')
                 break
         else:
             submit_button.click()
-            print('!!!!!! GOOD')
+            print(datetime.now())
             break
     try:
         final_submit = wait.until(ElementHasXpath(submit_xpath))
@@ -87,6 +96,7 @@ def run(username, password, cvv):
         pass
     else:
         final_submit.click()
+        print(datetime.now())
     return driver
 
 
